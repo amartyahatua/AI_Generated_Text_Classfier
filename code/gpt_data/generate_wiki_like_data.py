8fa0a56f3ec26adf53bc5900744e4c513be86ead
Amartya <amartyahatua@gmail.com>
2023-04-01 17:16:08 -0400

    Generate wiki like data using ChatGPT

--- code/gpt_data/generate_wiki_like_data.py
+++ code/gpt_data/generate_wiki_like_data.py
@@ -7,15 +7,22 @@ openai.api_key = API_KEY
 df = pd.read_csv('../../data/wiki_data_extract.csv')
 df['GPT_Generated_Text'] = ''*df.shape[0]
 
+result = pd.DataFrame()
 for i in tqdm(range(df.shape[0])):
+    excluded_strings = ['See also', 'References', 'External links']
+    if any(ext in df.iloc[i]['Section title'] for ext in excluded_strings):
+        print(df.iloc[i]['Section title'])
+        continue
+
     question_string = 'Describe {} {}'.format(df.iloc[i]['Page title'], df.iloc[i]['Section title'])
-    print(question_string)
     try:
         chatcompletion_response = openai.ChatCompletion.create(model="gpt-3.5-turbo",
             messages=[
                 {"role": "system", "content": question_string}
             ])
-        df.loc[i, 'GPT_Generated_Text'] = chatcompletion_response.to_dict()['choices'][0].to_dict()['message']['content']
-    except:
-        print('Error')
+        df.at[i, 'GPT_Generated_Text'] = chatcompletion_response.to_dict()['choices'][0].to_dict()['message']['content']
+    except Exception as e:
+        print('Error message: ', e)
+
 df.to_csv('../../data/chatgpt_generated_wiki_data.csv', index=False)
+
