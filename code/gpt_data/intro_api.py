4515501222e1417e2108d76e274b16074461a682
Amartya <amartyahatua@gmail.com>
2023-03-31 23:33:19 -0400

    'Adding intro for OpenAI API'

--- code/gpt_data/intro_api.py
+++ code/gpt_data/intro_api.py
@@ -1,11 +1,19 @@
-API_KEY = ''
+API_KEY = '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'
 
 
 import os
 import openai
 openai.api_key = API_KEY
 
+
+# Testing two APIs
 prompt = 'What is Boston?'
 
-response = openai.Completion.create(engine='text-davinci-001', prompt=prompt, max_tokens=6)
-print(response)
+completion_response = openai.Completion.create(engine='text-davinci-001', prompt=prompt, max_tokens=6)
+print(completion_response)
+
+chatcompletion_response = openai.ChatCompletion.create(model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system", "content": "What is Python Programing?"}
+    ])
+print(chatcompletion_response)
