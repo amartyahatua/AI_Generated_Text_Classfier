a99bf78ec67c2a72db4c0c7e8869620fe56c8071
Amartya <amartyahatua@gmail.com>
2023-04-04 22:47:20 -0400

    Wiki like data generation using GPT

---
+++ code/gpt_data/intro_api.py
@@ -0,0 +1,19 @@
+API_KEY = ''
+
+
+import os
+import openai
+openai.api_key = API_KEY
+
+
+# Testing two APIs
+prompt = 'What is Boston?'
+
+completion_response = openai.Completion.create(engine='text-davinci-001', prompt=prompt, max_tokens=6)
+print(completion_response)
+
+chatcompletion_response = openai.ChatCompletion.create(model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system", "content": "What is Python Programing?"}
+    ])
+print(chatcompletion_response)
