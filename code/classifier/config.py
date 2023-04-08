f85fe5a506321f9a116b8c4c3890fb594147a5d8
Amartya <amartyahatua@gmail.com>
2023-04-07 22:26:41 -0400

    Config function

---
+++ code/classifier/config.py
@@ -0,0 +1,2 @@
+
+data_source = '../../data/chatgpt_generated_wiki_data_1_5000.csv'
--- code/classifier/helpers.py
+++ code/classifier/helpers.py
@@ -1,12 +1,10 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-
+from AI_Generated_Text_Classfier.code.classifier.config import *
 def load_data():
     data = pd.DataFrame()
-    df = pd.read_csv('../../data/chatgpt_generated_wiki_data_1_5000.csv')
+    df = pd.read_csv(data_source)
     data['text'] = pd.concat((df['Text'],df['GPT_Generated_Text']),axis=0)
     data['label'] = pd.concat((pd.DataFrame([0]*df['Text'].shape[0]),pd.DataFrame([1]*df['GPT_Generated_Text'].shape[0])),axis=0)
-
-
 
 load_data()
