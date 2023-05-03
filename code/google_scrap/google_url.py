0e70dbca26a896a969ea8849e82cde8f06abc8ed
Amartya <amartyahatua@gmail.com>
2023-05-03 18:43:35 -0400

    adding urls for US Election 2024

--- code/google_scrap/google_url.py
+++ code/google_scrap/google_url.py
@@ -1,26 +1,25 @@
 try:
-	from googlesearch import search
-	import pandas as pd
+    from googlesearch import search
+    import pandas as pd
 except ImportError:
-	print("No module named 'google' found")
+    print("No module named 'google' found")
 
 # to search
 queries = ['US Election 2024', 'US Election 2024 in New York Times', 'US Election 2024 in Washington post',
-		   'US Election 2024 on CNN', 'US Election 2024 on Fox News Channel','US Election 2024 on ABC News',
-		   'US Election 2024 on CBS News', 'US Election 2024 on MSNBC', 'US Election 2024 on NBC News',
-		   'US Election 2024 on USA Today', 'US Election 2024 in The Wall Street Journal',
-		   'US Election 2024 in POLITICO', 'US Election 2024 in Bloomberg', 'US Election 2024 in Vice News',
-		   'US Election 2024 in HuffPost', 'US Election 2024 in  in Time Magazine',
-		   'US Election 2024 in U.S. News & World Report Magazine', 'US Election 2024 in in Newsweek Magazine']
+           'US Election 2024 on CNN', 'US Election 2024 on Fox News Channel', 'US Election 2024 on ABC News',
+           'US Election 2024 on CBS News', 'US Election 2024 on MSNBC', 'US Election 2024 on NBC News',
+           'US Election 2024 on USA Today', 'US Election 2024 in The Wall Street Journal',
+           'US Election 2024 in POLITICO', 'US Election 2024 in Bloomberg', 'US Election 2024 in Vice News',
+           'US Election 2024 in HuffPost', 'US Election 2024 in  in Time Magazine',
+           'US Election 2024 in U.S. News & World Report Magazine', 'US Election 2024 in in Newsweek Magazine']
 
 count = 0
-result = []
+
 for query in queries:
-	for j in search(query, pause=10.0,):
-		count += 1
-		result.append(j)
-		print('Number of URL got = ', count)
+    result = []
+    for j in search(query, pause=10.0, ):
+        result.append(j)
 
-result = pd.DataFrame(result, columns=['URL'])
-result.to_csv('../../data/url_us_election_2024.csv',index=False)
-
+    result = pd.DataFrame(result, columns=['URL'])
+    result.to_csv('../../data/url_us_election_2024.csv', mode='a', index=False)
+    print('Done for = ', query)
