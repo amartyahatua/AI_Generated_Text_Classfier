8beb109c3c18271b3c8f4adfcb7b1ca188036604
Amartya <amartyahatua@gmail.com>
2023-05-02 21:35:34 -0400

    adding urls for US Election 2024

--- code/google_scrap/google_url.py
+++ code/google_scrap/google_url.py
@@ -5,14 +5,16 @@ except ImportError:
 	print("No module named 'google' found")
 
 # to search
-query = "US Election 2024"
+queries = ["US Election 2024", 'US Election 2024 in New York Times', 'US Election 2024 in Washington post',
+		   'US Election 2024 on CNN', 'US Election 2024 on Fox News',]
 
 count = 0
 result = []
-for j in search(query):
-	count += 1
-	result.append(j)
-	print('Number of URL got = ', count)
+for query in queries:
+	for j in search(query):
+		count += 1
+		result.append(j)
+		print('Number of URL got = ', count)
 
 result = pd.DataFrame(result, columns=['URL'])
 result.to_csv('../../data/url_us_election_2024.csv',index=False)
