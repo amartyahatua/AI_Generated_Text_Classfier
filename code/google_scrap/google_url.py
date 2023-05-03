d91035c41ece7393d5b351ffb96287c7e608089f
Amartya <amartyahatua@gmail.com>
2023-05-02 22:09:12 -0400

    adding urls for US Election 2024

--- code/google_scrap/google_url.py
+++ code/google_scrap/google_url.py
@@ -5,13 +5,18 @@ except ImportError:
 	print("No module named 'google' found")
 
 # to search
-queries = ["US Election 2024", 'US Election 2024 in New York Times', 'US Election 2024 in Washington post',
-		   'US Election 2024 on CNN', 'US Election 2024 on Fox News',]
+queries = ['US Election 2024', 'US Election 2024 in New York Times', 'US Election 2024 in Washington post',
+		   'US Election 2024 on CNN', 'US Election 2024 on Fox News Channel','US Election 2024 on ABC News',
+		   'US Election 2024 on CBS News', 'US Election 2024 on MSNBC', 'US Election 2024 on NBC News',
+		   'US Election 2024 on USA Today', 'US Election 2024 in The Wall Street Journal',
+		   'US Election 2024 in POLITICO', 'US Election 2024 in Bloomberg', 'US Election 2024 in Vice News',
+		   'US Election 2024 in HuffPost', 'US Election 2024 in  in Time Magazine',
+		   'US Election 2024 in U.S. News & World Report Magazine', 'US Election 2024 in in Newsweek Magazine']
 
 count = 0
 result = []
 for query in queries:
-	for j in search(query):
+	for j in search(query, pause=10.0,):
 		count += 1
 		result.append(j)
 		print('Number of URL got = ', count)
