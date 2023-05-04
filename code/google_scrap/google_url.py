4a85847173ad461bd619212e805bcf836b97d330
Amartya <amartyahatua@gmail.com>
2023-05-03 22:49:33 -0400

    adding urls for US Election 2024

--- code/google_scrap/google_url.py
+++ code/google_scrap/google_url.py
@@ -5,18 +5,21 @@ except ImportError:
     print("No module named 'google' found")
 
 # to search
-queries = ['US Election 2024', 'US Election 2024 in New York Times', 'US Election 2024 in Washington post',
-           'US Election 2024 on CNN', 'US Election 2024 on Fox News Channel', 'US Election 2024 on ABC News',
-           'US Election 2024 on CBS News', 'US Election 2024 on MSNBC', 'US Election 2024 on NBC News',
-           'US Election 2024 on USA Today', 'US Election 2024 in The Wall Street Journal',
-           'US Election 2024 in POLITICO', 'US Election 2024 in Bloomberg', 'US Election 2024 in Vice News',
-           'US Election 2024 in HuffPost', 'US Election 2024 in  in Time Magazine',
+# 'US Election 2024', 'US Election 2024 in New York Times', 'US Election 2024 in Washington post',
+#            'US Election 2024 on CNN', 'US Election 2024 on Fox News Channel', 'US Election 2024 on ABC News',
+#            'US Election 2024 on CBS News', 'US Election 2024 on MSNBC', 'US Election 2024 on NBC News',
+#            'US Election 2024 on USA Today', 'US Election 2024 in The Wall Street Journal',
+#            'US Election 2024 in POLITICO', 'US Election 2024 in Bloomberg', 'US Election 2024 in Vice News',
+#            'US Election 2024 in HuffPost',
+
+queries = ['US Election 2024 in  in Time Magazine',
            'US Election 2024 in U.S. News & World Report Magazine', 'US Election 2024 in in Newsweek Magazine']
 
 count = 0
 
 for query in queries:
     result = []
+    print('Starting for = ', query)
     for j in search(query, pause=10.0):
         result.append(j)
 
