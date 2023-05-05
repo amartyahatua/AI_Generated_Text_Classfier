b0d7f26d9854f76b0be2bcb901fb125c389a18e4
Amartya <amartyahatua@gmail.com>
2023-05-04 20:10:49 -0400

    adding urls for US Election 2024

--- code/google_scrap/google_url.py
+++ code/google_scrap/google_url.py
@@ -1,6 +1,10 @@
 try:
-    from googlesearch import search
+    import csv
+    import requests
     import pandas as pd
+    from itertools import chain
+    from bs4 import BeautifulSoup
+    from googlesearch import search
 except ImportError:
     print("No module named 'google' found")
 
@@ -17,12 +21,44 @@ queries = ['US Election 2024 in  in Time Magazine',
 
 count = 0
 
-for query in queries:
-    result = []
-    print('Starting for = ', query)
-    for j in search(query, pause=10.0):
-        result.append(j)
+# for query in queries:
+#     result = []
+#     print('Starting for = ', query)
+#     for j in search(query, pause=10.0):
+#         result.append(j)
+#
+#     result = pd.DataFrame(result, columns=['URL'])
+#     result.to_csv('../../data/url_us_election_2024.csv', mode='a', index=False)
+#     print('Done for = ', query)
 
-    result = pd.DataFrame(result, columns=['URL'])
-    result.to_csv('../../data/url_us_election_2024.csv', mode='a', index=False)
-    print('Done for = ', query)
+links = pd.read_csv('../../data/url_us_election_2024.csv').values.tolist()
+
+# Get unique list
+links = list(chain.from_iterable(links))
+links = pd.DataFrame(set(links), columns=['URL'])
+
+link = links.iloc[0][0]
+r = requests.get(link)
+
+soup = BeautifulSoup(r.content, 'html5lib')
+quotes = []
+
+table = soup.find('div', attrs={'id':'all_quotes'})
+
+for row in table.findAll('div',
+                         attrs = {'class':'col-6 col-lg-3 text-center margin-30px-bottom sm-margin-30px-top'}):
+    quote = {}
+    quote['theme'] = row.h5.text
+    quote['url'] = row.a['href']
+    quote['img'] = row.img['src']
+    quote['lines'] = row.img['alt'].split(" #")[0]
+    quote['author'] = row.img['alt'].split(" #")[1]
+    quotes.append(quote)
+
+filename = '../../elecetion_2024_extracted_text.csv'
+with open(filename, 'w', newline='') as f:
+    w = csv.DictWriter(f,['theme','url','img','lines','author'])
+    w.writeheader()
+    for quote in quotes:
+        w.writerow(quote)
+
