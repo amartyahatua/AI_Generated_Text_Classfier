66972879ad48b17495d5041a8b92f4a08732f897
Amartya <amartyahatua@gmail.com>
2023-05-11 20:15:05 -0400

    Generate questions to generate US election news like data.

--- code/gpt_data/generate_us_election_2024_data.py
+++ code/gpt_data/generate_us_election_2024_data.py
@@ -6,5 +6,7 @@ from tqdm import tqdm
 openai.api_key = API_KEY
 
 data = pd.read_csv('../../data/extracted_data_us_election_2024.csv')
-keywords = data[' Keywords']
-question_string = 'What are questions can be formed using all the key words: '+keywords+' ?'
+
+for i in tqdm(range(data.shape[0])):
+    keywords = ', '.join(data.iloc[i]['Keywords'].tolist())
+    question_string = 'What are questions can be formed using all the key words: '+keywords+'?'
