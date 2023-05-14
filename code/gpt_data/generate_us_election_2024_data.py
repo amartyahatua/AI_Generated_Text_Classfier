5c287d45c3cba1d0dc5646219dca73a231ae8b5c
Amartya <amartyahatua@gmail.com>
2023-05-14 13:57:42 -0400

    Generate questions to generate US election news like data.

--- code/gpt_data/generate_us_election_2024_data.py
+++ code/gpt_data/generate_us_election_2024_data.py
@@ -9,7 +9,6 @@ openai.api_key = API_KEY
 data = pd.read_csv('../../data/extracted_data_us_election_2024.csv')
 
 
-
 def create_questions():
     for i in tqdm(range(data.shape[0])):
         keywords = data.iloc[i]['Keywords']
@@ -22,23 +21,25 @@ def create_questions():
 
             question_list = chatcompletion_response.to_dict()['choices'][0].to_dict()['message']['content'].split('\n')
 
-            #Get questio for every row and save in .csv file
-            temp = []
-            temp.append(data.iloc[i]['Title'])
-            temp.append(data.iloc[i]['Text'])
-            temp.append(data.iloc[i]['Summary'])
-            temp.append(keywords)
+            # Get question for every row and save in .csv file
+            temp = [data.iloc[i]['Title'], data.iloc[i]['Text'], data.iloc[i]['Summary'], keywords]
 
             for i in range(len(question_list)):
-                if(len(question_list[i].split('.')[-1].strip())):
+                if (len(question_list[i].split('.')[-1].strip())):
                     temp.append(question_list[i].split('.')[-1].strip())
             temp = pd.DataFrame([temp])
             temp.to_csv('../../data/chatgpt_generated_us_election_2024_questions.csv', mode='a', index=False,
-                                   header=False)
+                        header=False)
         except Exception as e:
             print('Error message: ', e)
             time.sleep(30)
             i = i - 1
 
 
+def generare_answers():
+    pass
+
+
+
 create_questions()
+generare_answers()
