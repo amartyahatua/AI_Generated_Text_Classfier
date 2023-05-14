b5b9052678734551aa3cac31f46b010750ae817f
Amartya <amartyahatua@gmail.com>
2023-05-13 23:10:38 -0400

    Generate questions to generate US election news like data.

--- code/gpt_data/generate_us_election_2024_data.py
+++ code/gpt_data/generate_us_election_2024_data.py
@@ -1,4 +1,4 @@
-API_KEY = ''
+API_KEY = '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'
 import time
 import pandas as pd
 import openai
@@ -7,11 +7,15 @@ from tqdm import tqdm
 openai.api_key = API_KEY
 
 data = pd.read_csv('../../data/extracted_data_us_election_2024.csv')
+data['GPT_Generated_Question_1'] = ''*data.shape[0]
+data['GPT_Generated_Question_2'] = ''*data.shape[0]
+data['GPT_Generated_Question_3'] = ''*data.shape[0]
+
 
 
 def create_questions():
     for i in tqdm(range(data.shape[0])):
-        keywords = ', '.join(data.iloc[i]['Keywords'].tolist())
+        keywords = data.iloc[i]['Keywords']
         question_string = 'What are questions can be formed using all the key words: ' + keywords + '?'
         try:
             chatcompletion_response = openai.ChatCompletion.create(model="gpt-3.5-turbo",
@@ -25,3 +29,4 @@ def create_questions():
             print('Error message: ', e)
             time.sleep(30)
             i = i-1
+create_questions()
