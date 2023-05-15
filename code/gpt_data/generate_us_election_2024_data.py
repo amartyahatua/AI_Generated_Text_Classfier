6a4d77fe5cf2281ac7553fc4b70609aeb0af5732
Amartya <amartyahatua@gmail.com>
2023-05-15 10:25:46 -0400

    Generate questions to generate US election news like data.

--- code/gpt_data/generate_us_election_2024_data.py
+++ code/gpt_data/generate_us_election_2024_data.py
@@ -11,33 +11,42 @@ data = pd.read_csv('../../data/extracted_data_us_election_2024.csv')
 
 def create_questions():
     for i in tqdm(range(data.shape[0])):
-        keywords = data.iloc[i]['Keywords']
-        question_string = 'What are questions can be formed using all the key words: ' + keywords + '?'
-        try:
-            chatcompletion_response = openai.ChatCompletion.create(model="gpt-3.5-turbo",
-                                                                   messages=[
-                                                                       {"role": "system", "content": question_string}
-                                                                   ])
+        if(i>1189):
+            try:
+                keywords = data.iloc[i]['Keywords']
+                if keywords is  not None:
+                    question_string = 'Give me 10 questions using the following key words: ' + keywords + '?'
+                    chatcompletion_response = openai.ChatCompletion.create(model="gpt-3.5-turbo",
+                                                                           messages=[
+                                                                               {"role": "system", "content": question_string}
+                                                                           ])
 
-            question_list = chatcompletion_response.to_dict()['choices'][0].to_dict()['message']['content'].split('\n')
+                    question_list = chatcompletion_response.to_dict()['choices'][0].to_dict()['message']['content'].split('\n')
 
-            # Get question for every row and save in .csv file
-            temp = [data.iloc[i]['Title'], data.iloc[i]['Text'], data.iloc[i]['Summary'], keywords]
+                    # Get question for every row and save in .csv file
+                    temp = [data.iloc[i]['Title'], data.iloc[i]['Text'], data.iloc[i]['Summary'], keywords]
 
-            for i in range(len(question_list)):
-                if (len(question_list[i].split('.')[-1].strip())):
-                    temp.append(question_list[i].split('.')[-1].strip())
-            temp = pd.DataFrame([temp])
-            temp.to_csv('../../data/chatgpt_generated_us_election_2024_questions.csv', mode='a', index=False,
-                        header=False)
-        except Exception as e:
-            print('Error message: ', e)
-            time.sleep(30)
-            i = i - 1
+                    for i in range(len(question_list)):
+                        if len(question_list[i].split('.')[-1].strip()):
+                            temp.append(question_list[i].split('.')[-1].strip())
+                    temp = pd.DataFrame([temp])
+                    temp.to_csv('../../data/chatgpt_generated_us_election_2024_questions.csv', mode='a', index=False,
+                                header=False)
+            except Exception as e:
+                print('Error message: ', e)
+                time.sleep(30)
+                i = i - 1
 
 
 def generare_answers():
-    pass
+    data = pd.read_csv('../../data/chatgpt_generated_us_election_2024_questions.csv')
+    for i in tqdm(range(data.shape[0])):
+        row_data = data.iloc[i].to_list()[4:-1]
+        print('New article')
+        for count in range(len(row_data)):
+            if row_data[count] is not None:
+                print(row_data[count])
+
 
 
 
