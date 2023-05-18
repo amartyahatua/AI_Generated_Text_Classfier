7de4b5bf1073f522d513f724dfb02c83c59337f8
Amartya <amartyahatua@gmail.com>
2023-05-17 23:39:10 -0400

    Document similarity calculations using sentence transformer embeddings and cosine similarities

--- code/analysis/document_similarity.py
+++ code/analysis/document_similarity.py
@@ -1,38 +1,69 @@
 # source:https://medium.com/@adriensieg/text-similarities-da019229c894
+
+import pandas as pd
 import numpy as np
-import scipy
+from nltk.corpus import stopwords
+#import nltk
 
-def jaccard_similarity(query, document):
-    intersection = set(query).intersection(set(document))
-    union = set(query).union(set(document))
-    return len(intersection) / len(union)
+#nltk.download('stopwords')
+import re
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.metrics.pairwise import euclidean_distances
+from sentence_transformers import SentenceTransformer
+from tqdm import tqdm
 
+model = SentenceTransformer('bert-base-nli-mean-tokens')
 
-def cosine_distance_wordembedding_method(s1, s2):
-    vector_1 = np.mean([model[word] for word in preprocess(s1)], axis=0)
-    vector_2 = np.mean([model[word] for word in preprocess(s2)], axis=0)
-    cosine = scipy.spatial.distance.cosine(vector_1, vector_2)
-    print('Word Embedding method with a cosine distance asses that our two sentences are similar to',
-          round((1 - cosine) * 100, 2), '%')
+us_election_qa = pd.read_csv('../../data/chatgpt_generated_us_election_2024_questions_answers_combine.csv')
 
+for i in tqdm(range(us_election_qa.shape[0])):
+    if i > 0:
+        break
+    compare_embedd = []
 
-def jensen_shannon(query, matrix):
-    """
-    This function implements a Jensen-Shannon similarity
-    between the input query (an LDA topic distribution for a document)
-    and the entire corpus of topic distributions.
-    It returns an array of length M where M is the number of documents in the corpus
-    """
-    # lets keep with the p,q notation above
-    p = query[None,:].T # take transpose
-    q = matrix.T # transpose matrix
-    m = 0.5*(p + q)
-    return np.sqrt(0.5*(entropy(p,m) + entropy(q,m)))
+    row_text = us_election_qa.iloc[i]['Text']
+    row_text_embd = model.encode(row_text)
 
-def get_most_similar_documents(query,matrix,k=10):
-    """
-    This function implements the Jensen-Shannon distance above
-    and retruns the top k indices of the smallest jensen shannon distances
-    """
-    sims = jensen_shannon(query,matrix) # list of jensen shannon distances
-    return sims.argsort()[:k] # the top k positional index of the smallest Jensen Shannon distances
+    row_ans1 = us_election_qa.iloc[i]['Answer 1']
+    row_ans1_embd = model.encode(row_ans1)
+    compare_embedd.append(row_ans1_embd)
+
+    row_ans2 = us_election_qa.iloc[i]['Answer 2']
+    row_ans2_embd = model.encode(row_ans2)
+    compare_embedd.append(row_ans2_embd)
+
+    row_ans3 = us_election_qa.iloc[i]['Answer 3']
+    row_ans3_embd = model.encode(row_ans3)
+    compare_embedd.append(row_ans3_embd)
+
+    row_ans4 = us_election_qa.iloc[i]['Answer 4']
+    row_ans4_embd = model.encode(row_ans4)
+    compare_embedd.append(row_ans4_embd)
+
+    row_ans5 = us_election_qa.iloc[i]['Answer 5']
+    row_ans5_embd = model.encode(row_ans5)
+    compare_embedd.append(row_ans5_embd)
+
+    row_ans6 = us_election_qa.iloc[i]['Answer 6']
+    row_ans6_embd = model.encode(row_ans6)
+    compare_embedd.append(row_ans6_embd)
+
+    row_ans7 = us_election_qa.iloc[i]['Answer 7']
+    row_ans7_embd = model.encode(row_ans7)
+    compare_embedd.append(row_ans7_embd)
+
+    row_ans8 = us_election_qa.iloc[i]['Answer 8']
+    row_ans8_embd = model.encode(row_ans8)
+    compare_embedd.append(row_ans8_embd)
+
+    row_ans9 = us_election_qa.iloc[i]['Answer 9']
+    row_ans9_embd = model.encode(row_ans9)
+    compare_embedd.append(row_ans9_embd)
+
+    row_ans10 = us_election_qa.iloc[i]['Answer 10']
+    row_ans10_embd = model.encode(row_ans10)
+    compare_embedd.append(row_ans10_embd)
+
+similarities = cosine_similarity([row_text_embd], compare_embedd)
+print('pairwise dense output:\n {}\n'.format(similarities))
