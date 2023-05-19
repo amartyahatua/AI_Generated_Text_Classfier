b4df6309beee66a7afbea9609d3f9dee1ce0f081
Amartya <amartyahatua@gmail.com>
2023-05-19 17:58:37 -0400

    Document similarity calculations using sentence transformer embeddings and cosine similarities

--- code/analysis/document_similarity.py
+++ code/analysis/document_similarity.py
@@ -65,35 +65,18 @@ def cosine_similarity_sentence_transformer():
                                                       'Cos Similarity 9', 'Cos Similarity 10'])
             similarities = pd.concat([similarities_temp, similarities], axis=0)
         except:
+            similarities_temp = pd.DataFrame([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]],
+                                             columns=['Cos Similarity 1', 'Cos Similarity 2', 'Cos Similarity 3',
+                                                      'Cos Similarity 4',
+                                                      'Cos Similarity 5', 'Cos Similarity 6', 'Cos Similarity 7',
+                                                      'Cos Similarity 8',
+                                                      'Cos Similarity 9', 'Cos Similarity 10'])
+            similarities = pd.concat([similarities_temp, similarities], axis=0)
+            print('Here')
             continue
 
     similarities = similarities.reset_index(drop=True)
-
-    us_election_qa_smilarity = pd.concat(
-        [us_election_qa[['Title', 'Text', 'Summary', 'Keywords', 'Question 1', 'Answer 1']],
-         similarities['Cos Similarity 1'],
-         us_election_qa[['Question 2', 'Answer 2']],
-         similarities['Cos Similarity 2'],
-         us_election_qa[['Question 3', 'Answer 3']],
-         similarities['Cos Similarity 3'],
-         us_election_qa[['Question 4', 'Answer 4']],
-         similarities['Cos Similarity 4'],
-         us_election_qa[['Question 5', 'Answer 5']],
-         similarities['Cos Similarity 5'],
-         us_election_qa[['Question 6', 'Answer 6']],
-         similarities['Cos Similarity 6'],
-         us_election_qa[['Question 7', 'Answer 7']],
-         similarities['Cos Similarity 7'],
-         us_election_qa[['Question 8', 'Answer 8']],
-         similarities['Cos Similarity 8'],
-         us_election_qa[['Question 9', 'Answer 9']],
-         similarities['Cos Similarity 9'],
-         us_election_qa[['Question 10', 'Answer 10']],
-         similarities['Cos Similarity 10']], axis=1)
-
-    print(us_election_qa_smilarity.shape)
-    us_election_qa_smilarity.to_csv('../../data/chatgpt_generated_us_election_2024_questions_answers_similarity.csv',
-                                    index=False)
+    return  similarities
 
 
 def jaccard_similarity(doc1, doc2):
@@ -114,10 +97,8 @@ def jaccard_similarity(doc1, doc2):
 
 def find_jaccard_similarity():
     similarities = pd.DataFrame()
+    us_election_qa = pd.read_csv('../../data/chatgpt_generated_us_election_2024_questions_answers_combine.csv')
     for i in tqdm(range(us_election_qa.shape[0])):
-
-        if (i > 3):
-            break
         try:
             temp_similarity = []
             ai_gen_text = []
@@ -160,6 +141,7 @@ def find_jaccard_similarity():
                 try:
                     temp_similarity.append(jaccard_similarity(row_text, ai_gen_text[i]))
                 except:
+                    temp_similarity.append(0)
                     print('Here')
 
             temp_similarity = pd.DataFrame([temp_similarity],
@@ -174,5 +156,37 @@ def find_jaccard_similarity():
     return similarities
 
 
-cosine_similarity_sentence_transformer()
-find_jaccard_similarity()
+cosine_similarity = cosine_similarity_sentence_transformer()
+jaccard_similarity = find_jaccard_similarity()
+
+us_election_qa_smilarity = pd.concat(
+    [us_election_qa[['Title', 'Text', 'Summary', 'Keywords', 'Question 1', 'Answer 1']],
+     cosine_similarity['Cos Similarity 1'], jaccard_similarity['JAC Similarity 1'],
+     us_election_qa[['Question 2', 'Answer 2']],
+     cosine_similarity['Cos Similarity 2'], jaccard_similarity['JAC Similarity 2'],
+     us_election_qa[['Question 3', 'Answer 3']],
+     cosine_similarity['Cos Similarity 3'], jaccard_similarity['JAC Similarity 3'],
+     us_election_qa[['Question 4', 'Answer 4']],
+     cosine_similarity['Cos Similarity 4'], jaccard_similarity['JAC Similarity 4'],
+     us_election_qa[['Question 5', 'Answer 5']],
+     cosine_similarity['Cos Similarity 5'], jaccard_similarity['JAC Similarity 5'],
+     us_election_qa[['Question 6', 'Answer 6']],
+     cosine_similarity['Cos Similarity 6'], jaccard_similarity['JAC Similarity 6'],
+     us_election_qa[['Question 7', 'Answer 7']],
+     cosine_similarity['Cos Similarity 7'], jaccard_similarity['JAC Similarity 7'],
+     us_election_qa[['Question 8', 'Answer 8']],
+     cosine_similarity['Cos Similarity 8'], jaccard_similarity['JAC Similarity 8'],
+     us_election_qa[['Question 9', 'Answer 9']],
+     cosine_similarity['Cos Similarity 9'], jaccard_similarity['JAC Similarity 9'],
+     us_election_qa[['Question 10', 'Answer 10']],
+     cosine_similarity['Cos Similarity 10'], jaccard_similarity['JAC Similarity 10'],], axis=1)
+
+# print(us_election_qa_smilarity.shape)
+# cosine_similarity.to_csv('../../data/chatgpt_generated_us_election_2024_questions_answers_cosine_similarity.csv',
+#                                 index=False)
+
+# jaccard_similarity.to_csv('../../data/chatgpt_generated_us_election_2024_questions_answers_jaccard_similarity.csv',
+#                                 index=False)
+
+us_election_qa_smilarity.to_csv('../../data/chatgpt_generated_us_election_2024_questions_answers_similarity.csv',
+                                index=False)
